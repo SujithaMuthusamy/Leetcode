c3d183641894837401ec6605cc89aefbd03a7560
SujithaMuthusamy <sujiieee.05@gmail.com>
2026-06-16 20:48:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (66.09%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.c
@@ -0,0 +1,22 @@
+void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n) {
+    int merged[m+n];
+    int k=0;
+    for(int i=0;i<m;i++){
+        merged[k++]=nums1[i];
+    }
+    for(int j=0;j<n;j++){
+        merged[k++]=nums2[j];
+    }
+    for(int i=0;i<m+n-1;i++){
+        for(int j=i+1;j<m+n;j++){
+            if(merged[i]>merged[j]){
+                int temp=merged[i];
+                merged[i]=merged[j];
+                merged[j]=temp;
+            }
+        }
+    }
+    for(int i=0;i<m+n;i++){
+        nums1[i]=merged[i];
+    }
+}
