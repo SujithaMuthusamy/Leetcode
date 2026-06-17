a0f4055556a44b02220626c9f4407fd2034e4d5c
SujithaMuthusamy <sujiieee.05@gmail.com>
2026-06-17 22:02:16 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (38.21%) - LeetSync

---
+++ 169-majority-element/majority-element.c
@@ -0,0 +1,18 @@
+int majorityElement(int* nums, int numsSize) {
+    int current=0;
+    int count=0;
+    for(int i=0;i<numsSize;i++){
+        if(count==0){
+            current=nums[i];
+        }
+        if(current==nums[i]){
+            count++;
+        }
+        else{
+            count--;
+        }
+    }
+        return current;
+    
+    
+}
